fe4b7e978f55f1b2358536f1218912b05c20cf2a
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-01-27 09:23:52 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict"
+
+// Declare two numbers
+let number1 = 5; // The first number
+let number2 = 10; // The second number
+
+// Add the two numbers together and store the result in a new variable
+let sum = number1 + number2; // Sum of number1 and number2
